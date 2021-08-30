a592926efbcd99ec61987b10cc3f4b058fc5e926
AbdullahELian1 <abodelian28@gmail.com>
2021-08-30 13:54:15 +0300

    add first test to check the status for snacks_list

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
+# Create your tests here.
+class thingsTests(TestCase):
+    def test_home_page_status_code(self):
+        url = reverse('snacks_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+   
 # Create your tests here.
